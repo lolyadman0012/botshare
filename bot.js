ab50f77a4fbb90ab9b0e49c4630ef9c3849d2891
lolyadman0012 <41803482+lolyadman0012@users.noreply.github.com>
2019-02-15 02:00:47 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,19 @@
+﻿const Discord = require('discord.js');
+const A7MD = new Discord.Client();
+
+console.log("BOT ONLINE");
+ 
+A7MD.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(` 
+**
+سيرفرنا يرحب بك يا جميل لازم تدخل
+
+<https://discord.gg/FhRSeRv>
+
+:heart:
+
+**`) 
+}).catch(console.error)
+})
+A7MD.login("NTQ1NjcyNDEwNjMyODE0NTky.D0dNjA.RMrFc3hpZ0_tKMvFUTFTYg_Rz50");
